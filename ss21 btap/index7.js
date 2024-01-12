891d664a8d9aa8c9ac3cfab2ffa98cf1fd0a0c88
auductuan <147390394+auductuan@users.noreply.github.com>
2024-01-12 13:02:57 +0700

    Add files via upload

---
+++ ss21 btap/index7.js
@@ -0,0 +1,12 @@
+let a= prompt("Nhập số a bất kì");
+let b= prompt("Nhập số b bất kì");
+let c= prompt("Nhập số c bất kì");
+if ( a>b ){
+    console.log("So a la so lon nhat");
+}else if(b > c){
+    console.log("So b la so lon nhat");
+}else if(c > a){
+    console.log("So c la so lon nhat");
+}else{
+    console.log("kh hop le")
+}
